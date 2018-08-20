90f58630e4fdcb87f0902c741d03029aaef080c2
RuairiF <rf17160@bris.ac.uk>
2018-08-20 21:54:24 +0100

    Minimum Working

--- Compiler.py
+++ Compiler.py
@@ -2,38 +2,106 @@ from PIL import Image
 
 condreg = 0
 stack = []
+
+# single callstack so you really should have functions terminate in call order
+# or v bad
 callstack = []
-callstack2 = []
-callstack3 = []
-callstack4 = []
-pointers = [(0,0)]
-#name = raw_input("What is the name of your image? ")
+
+pointers = [(0, 0)]
+#name = raw_input("What is the name of your code? ")
 name = "test.png"
 im = Image.open(name)
 pix = im.load()
 width, height = im.size
 
+# No operation
 
-def operate(opcode, operand):
+
+def nop(operand):
     pass
 
+# Pushes operand to the stack
+
+
+def push(operand):
+    stack.insert(0, operand)
+
+# Pops and prints the first element of the stack, Prints as ascii char if non-zero operand
+
+
+def pop(operand):
+    out = stack.pop(0)
+    if operand != 0:
+        print chr(out)
+    else:
+        print out
+
+# Adds top elements of stack unless given non zero arg
+
+
+def add(operand):
+    if operand == 0:
+        stack.insert(0, stack.pop(0) + stack.pop(0))
+    else:
+        stack.insert(0, stack.pop(0) + operand)
+
+# Subtracts top elements of stack, unless given non zero arg
+
+
+def sub(operand):
+    if operand == 0:
+        stack.insert(0, stack.pop(0) - stack.pop(0))
+    else:
+        stack.insert(0, stack.pop(0) - operand)
+
+# Jumps to (stack[0],stack[1])
+
+
+def jump(operand):
+    global pointers
+    try:
+        pointers.append(stack.pop(0), stack.pop(0))
+    except IndexError:
+        print "Tried to pop empty stack"
+        exit(1)
+
+# transforms current coordinates to have opcode operrand and operand stack.pop()
+
+
+def transform(operand):
+    global x, y, pix
+    pix[(x, y)] = (operand, stack.pop(0), pix[(x, y)][3])
+
+
+functions = {0: nop, 1: push, 2: pop, 3: add, 4: sub, 5: jump, 6: transform}
+
+
+def operate(opcode, operand):
+    try:
+        functions[opcode](operand)
+    except KeyError:
+        print "R value not valid!"
+        exit(1)
+
+
 while pointers != []:
-    x, y = pointers.pop()
-    current = pix[x,y]
-    print current
+    global x, y
+    x, y = pointers.pop(0)
+    current = pix[x, y]
 
-    operate(current[0],current[1])
 
-    #Adds new pointers based of the B in RGB code.
-    #1111 = WSEN
+    operate(current[0], current[1])
 
+    # Adds new pointers based of the B in RGB code.
+    # 1111 = WSEN
+    
     if current[2] & 0b1:
-        pointers.append((x,y-1))
+        pointers.append((x, y-1))
     if current[2] >> 1 & 0b1:
-        pointers.append((x+1,y))
+        pointers.append((x+1, y))
     if current[2] >> 2 & 0b1:
-        pointers.append((x,y+1))
+        pointers.append((x, y+1))
     if current[2] >> 3 & 0b1:
-        pointers.append((x-1,y))
-        
-    print current
+        pointers.append((x-1, y))
+
+
--- Creator.py
+++ Creator.py
@@ -1,13 +1,33 @@
 from PIL import Image
 
+#functions = {0: nop, 1: push, 2: pop, 3: add, 4: sub, 5: jump, 6: transform}
+# 1111 = WSEN
+
 im = Image.open('blank.jpg')
 pix = im.load()
 
-pix[0,0] = (127,0,2)
-pix[1,0] = (0,127,4)
-pix[1,1] = (64,46,8)
-pix[0,1] = (46,64,1)
+pix[1,3] = (1,ord('h'),4)
+pix[2,2] = (1,ord('e'),0)
+pix[1,2] = (1,ord('l'),6)
+pix[0,2] = (1,ord('l'),2)
+pix[2,1] = (1,ord('o'),0)
+pix[1,1] = (1,ord('w'),2)
+pix[2,0] = (1,ord('o'),0)
+pix[0,1] = (1,ord('r'),6)
+pix[1,0] = (1,ord('l'),2)
+pix[0,0] = (1,ord('d'),6)
 
-print pix[0,0]
+pix[1,4] = (2,1,14)
+pix[0,4] = (2,20,0)
+pix[2,4] = (2,20,0)
+pix[1,5] = (2,20,14)
+pix[0,5] = (2,30,4)
+pix[2,5] = (2,30,6)
+pix[1,6] = (2,30,0)
+pix[0,6] = (2,40,0)
+pix[2,6] = (2,40,0)
+pix[3,5] = (2,40,0)
+
+
 
 im.save('test.png',quality = 95)
